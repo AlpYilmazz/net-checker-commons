b66dd5a2bfe9da15ac3356ac94ffa1d636b7dcc1
Alperen Yılmaz <yilmazmuha17@itu.edu.tr>
2023-08-14 01:15:10 +0300

    Dev

--- src/message.rs
+++ src/message.rs
@@ -1,20 +1,46 @@
+use pulsar::{DeserializeMessage, Payload};
 use serde::{Deserialize, Serialize};
 
 use crate::{MonitorConfiguration, MonitorTimingSettings};
 
 #[derive(Serialize, Deserialize)]
+pub enum MonitorMessage {
+    Create(CreateMonitorMessage),
+    Pause(PauseMonitorMessage),
+    Resume(ResumeMonitorMessage),
+    Kill(KillMonitorMessage),
+}
+
+impl DeserializeMessage for MonitorMessage {
+    type Output = Result<Self, serde_json::Error>;
+
+    fn deserialize_message(payload: &Payload) -> Self::Output {
+        serde_json::from_slice(&payload.data)
+    }
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct CreateMonitorMessage {
+    pub monitor_id: String,
     pub timing: MonitorTimingSettings,
     pub monitor: MonitorConfiguration,
 }
 
 #[derive(Serialize, Deserialize)]
-pub enum ChangeMonitorStateMessage {
-    Start { monitor_id: String },
-    Stop { monitor_id: String },
+pub struct PauseMonitorMessage {
+    pub monitor_id: String,
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct DeleteMonitorMessage {
+pub struct ResumeMonitorMessage {
     pub monitor_id: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct KillMonitorMessage {
+    pub monitor_id: String,
+}
+
+pub fn topic(act: &str, worker_id: &str) -> String {
+    format!("persistent://public/net-monitor/{act}_{worker_id}")
 }
